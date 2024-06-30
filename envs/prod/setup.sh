4cca43594c163de33b4057b769d9f2cd248cd096
Richard Marques <richard.filho@seberino.com.br>
2024-06-30 20:01:15 -0300

    fix: setup sh fixed

--- envs/prod/setup.sh
+++ envs/prod/setup.sh
@@ -4,5 +4,8 @@ gcloud config set project obd-prod
 gcloud iam service-accounts create sa-pipeline \
     --display-name "Service Account for pipeline execution"
 
-gcloud iam service-accounts add-iam-policy-binding sa-pipeline@obd-prod.iam.gserviceaccount.com \
-    member='serviceAccount:sa-pipeline@obd-prod.iam.gserviceaccount.com' --role='roles/editor'
+gcloud projects add-iam-policy-binding obd-prod \
+    --member='serviceAccount:sa-pipeline@obd-prod.iam.gserviceaccount.com' --role='roles/editor'
+
+gcloud projects add-iam-policy-binding obd-prod \
+    --member='serviceAccount:sa-pipeline@obd-prod.iam.gserviceaccount.com' --role='roles/logging.logWriter'
--- envs/stage/setup.sh
+++ envs/stage/setup.sh
@@ -4,5 +4,8 @@ gcloud config set project obd-stage
 gcloud iam service-accounts create sa-pipeline \
     --display-name "Service Account for pipeline execution"
 
-gcloud iam service-accounts add-iam-policy-binding sa-pipeline@obd-stage.iam.gserviceaccount.com \
+gcloud projects add-iam-policy-binding obd-stage \
     --member='serviceAccount:sa-pipeline@obd-stage.iam.gserviceaccount.com' --role='roles/editor'
+
+gcloud projects add-iam-policy-binding obd-stage \
+    --member='serviceAccount:sa-pipeline@obd-stage.iam.gserviceaccount.com' --role='roles/logging.logWriter'
